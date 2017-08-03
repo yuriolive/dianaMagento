966fd5ff73c214742cd685fb2e7d16e6d8671f32
Yuri Olive <yuriso1994@gmail.com>
2017-08-03 16:31:27 -0300

    folders

--- scripts/00-nginxconfig.sh
+++ scripts/00-nginxconfig.sh
@@ -1,4 +1,2 @@
 rm /etc/nginx/sites-available/default.conf
 mv /var/www/html/conf/nginx-site.conf /etc/nginx/sites-available/default.conf
-rm /etc/nginx/sites-available/default-ssl.conf
-mv /var/www/html/conf/nginx-site-ssl.conf /etc/nginx/sites-available/default.conf
