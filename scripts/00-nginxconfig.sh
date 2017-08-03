1d111ff0b455708b9d59ca33dbea3089c0a4561e
Yuri Olive <yuriso1994@gmail.com>
2017-08-03 16:36:49 -0300

    nginx 2 try

--- scripts/00-nginxconfig.sh
+++ scripts/00-nginxconfig.sh
@@ -1,2 +1,7 @@
-rm /etc/nginx/sites-available/default.conf
-mv /var/www/html/conf/nginx-site.conf /etc/nginx/sites-available/default.conf
+if [ -f /etc/nginx/sites-available/default-ssl.conf ]; then
+	rm /etc/nginx/sites-available/default-ssl.conf
+	mv /var/www/html/conf/nginx-site-ssl.conf /etc/nginx/sites-available/default.conf
+else
+	rm /etc/nginx/sites-available/default.conf
+	mv /var/www/html/conf/nginx-site.conf /etc/nginx/sites-available/default.conf
+fi
